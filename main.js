73ca21747222c3a7c64ddceb5fa5479d2e4b5a5c
thecodersourabh <bmsourabhjoshi@gmail.com>
2024-11-08 17:39:02 +0530

    updated a lab live dist

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-(self["webpackChunkmypedia"] = self["webpackChunkmypedia"] || []).push([["main"],{
+(self["webpackChunkalab"] = self["webpackChunkalab"] || []).push([["main"],{
 
 /***/ 4114:
 /*!***************************************!*\
